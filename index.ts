2f125e4f6419ac45243628148a04d678194c9c7a
iUnstable0 <iunstable0@pm.me>
2024-10-19 19:09:36 +0700

    Update

--- index.ts
+++ index.ts
@@ -312,10 +312,10 @@ app.post("/user/checkout", async (req, res) => {
 
     // console.log(checkoutActions);
 
-    const url = "http://localhost:8081";
+    const url = "http://127.0.0.1:8081";
 
-    const url_2 = "http://localhost:8082/dmuser";
-
+    const url_2 = "http://127.0.0.1:8082/dmuser";
+    s;
     const data = {
       playeruuid,
       commands: [
